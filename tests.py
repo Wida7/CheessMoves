0c2b61a3e6103163a659cc081c8f306633892944
Tu Nombre <you@example.com>
2019-05-07 20:03:08 -0500

    Avance clase 1

--- tests.py
+++ tests.py
@@ -18,5 +18,6 @@ class Test_movimientos(TestCase):
     def test_mover_torre(self):
         dado = []
         espero = []
-        obtengo = mover_torre(dado[0], dado[1], dado[2] , dado[3], dado[4])
+        obtengo = mover_torre(dado[0], dado[1], dado[2], dado[3], dado[4])
         self.assertEquals(espero, obtengo)
+
